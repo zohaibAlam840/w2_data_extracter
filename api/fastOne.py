545b09b062fda63cd9a9dc8ae3aa88dff4f228e4
Zohaib Alam Pervaiz <ikillar840@gmail.com>
2024-09-26 11:22:17 +0500

    pulling

--- api/fastOne.py
+++ api/fastOne.py
@@ -51,7 +51,7 @@ app = FastAPI()
 # CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
